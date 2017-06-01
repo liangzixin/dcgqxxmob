6bd850131a64584fb3f39fbdc233131f36ab21f7
liangzixin <648194240@qq.com>
2017-06-02 01:01:35 +0800

    2017050201

--- aT/src/main/java/com/xiangmu/lzx/utils/HttpUtil.java
+++ aT/src/main/java/com/xiangmu/lzx/utils/HttpUtil.java
@@ -1,6 +1,6 @@
 package com.xiangmu.lzx.utils;
 public class HttpUtil {
 	//public static final String BASE_URL = "http://www.dcgqxx.com/";
-//public static final String BASE_URL = "http://192.168.1.18:8086/dcgqxx/";
-	public static final String BASE_URL = "http://192.168.16.101:8086/dcgqxx/";
+public static final String BASE_URL = "http://192.168.1.18:8086/dcgqxx/";
+	//public static final String BASE_URL = "http://192.168.16.101:8086/dcgqxx/";
 }
