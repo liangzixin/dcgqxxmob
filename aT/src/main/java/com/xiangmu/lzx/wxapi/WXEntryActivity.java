81ca89bf367323bc4aa007ae1854efde6f9fc403
liangzixin <648194240@qq.com>
2017-06-27 10:52:47 +0800

    2017062703

---
+++ aT/src/main/java/com/xiangmu/lzx/wxapi/WXEntryActivity.java
@@ -0,0 +1,6 @@
+package com.xiangmu.lzx.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
---
+++ photolib1/build/generated/source/buildConfig/release/com/litao/android/lib/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.litao.android.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.litao.android.lib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
